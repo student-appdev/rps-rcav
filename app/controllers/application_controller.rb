b817e12dadaf0b655697ebdcb0b07d710f50f43b
AsyaGr <agranovs@chicagobooth.edu>
2022-02-21 20:18:43 +0000

    commit 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+def play_rock
+  #redirect_to ("https://en.wikipedia.org/wiki/English_Wikipedia")
+  #render({ :html => "<h1>Howdy, world!</h1>".html_safe})
+  render({ :template => "game_templates/user_rock.html.erb"})
+end
+
 
 end
